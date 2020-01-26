26c44253af023b9456e4e726d751e394e2d58b04
Faisal Alam <faisal.alam@rentomojo.com>
2020-01-27 01:15:40 +0530

    Add JSDoc

--- database/index.js
+++ database/index.js
@@ -6,4 +6,11 @@ const path = require('path');
 
 const driver = config.database.driver;
 
+/**
+ * @type {{
+ * saveRequest: (function(): string),
+ * saveOutput: (function(id: string, output: {stdout: string, stderr: string, status?: string}): Promise<*>)
+ * findRequestById: (function(id: string): boolean|{id: string, stdout: string, stderr: string, status: string})
+ * }}
+ */
 module.exports = require(path.join(__dirname, driver, 'index'));
