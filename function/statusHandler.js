510396ee0581d1373784b2d5b551035a5cb5822b
Faisal Alam <faisal.alam@rentomojo.com>
2020-01-28 22:24:15 +0530

    Add Cloud Function handler for task status update

---
+++ function/statusHandler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const ide = require('../src/ide/ide-requests');
+
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+module.exports = async (event, context) => {
+  const pubsubMessage = Buffer.from(event.data, 'base64').toString();
+
+  /**
+   * @typedef {{id: string, lang: string, source: string, stdin: string}} Job
+   * @type {{job: Job, stdout: string, stderr: string, compile_stderr: string, isTLE: boolean}}
+   */
+  const data = JSON.parse(pubsubMessage);
+  const job = data.job;
+
+  const payload = {
+    id: job.id,
+    stdout: data.stdout,
+    stderr: data.stderr,
+    compile_stderr: data.compile_stderr,
+    isTLE: data.isTLE
+  };
+
+  return ide.updateIDERequest(payload);
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.statusHandler = require('./function/statusHandler');
