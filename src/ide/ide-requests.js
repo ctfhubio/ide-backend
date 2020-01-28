e5ef1abdc56fc1c747ede5510f9a20245152acae
Faisal Alam <faisal.alam@rentomojo.com>
2020-01-28 16:55:50 +0530

    Add method `updateIDERequest`

--- src/ide/ide-requests.js
+++ src/ide/ide-requests.js
@@ -6,6 +6,8 @@ const queue = require('../queue');
 const errors = require('../errors');
 const validator = require('./ide-requests-validator');
 
+const STATUS = require('../../constants/ide-request-constants');
+
 module.exports = {
   saveRequest: async input => {
     const { source, stdin, lang } = await validator.saveRequest(input);
@@ -35,6 +37,41 @@ module.exports = {
       return Promise.reject(errors.ERR_REQUEST_NOT_FOUND.error());
     }
 
-    return data;
+    return {
+      id: data._id,
+      stdout: data.stdout,
+      stderr: data.stderr,
+      compile_stderr: data.compile_stderr,
+      status: data.status
+    };
+  },
+
+  /**
+   * @param {{id: id, stdout: string, stderr: string, compile_stderr: string, isTLE: boolean}} data
+   * @return {Promise<*>}
+   */
+  updateIDERequest: async data => {
+    const id = data.id;
+    const ideRequest = await db.findRequestById(id);
+
+    if (!ideRequest) {
+      return Promise.reject(errors.ERR_REQUEST_NOT_FOUND.error());
+    }
+
+    let status = STATUS.SUCCESS;
+    if (data.compile_stderr) {
+      status = STATUS.COMPILE_ERROR;
+    } else if (data.isTLE) {
+      status = STATUS.TIMEOUT;
+    } else if (data.stderr) {
+      status = STATUS.RUNTIME_ERROR;
+    }
+
+    ideRequest.stdout = data.stdout;
+    ideRequest.stderr = data.stderr;
+    ideRequest.compile_stderr = data.compile_stderr;
+    ideRequest.status = status;
+
+    return ideRequest.save();
   }
 };
